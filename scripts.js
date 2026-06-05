917819c1ea799aa0b3614b225b70ad17166c5123
Rafael <rafagsilva1312@gmail.com>
2026-06-04 22:33:22 -0300

    New Feature: Local Storage Added

--- scripts.js
+++ scripts.js
@@ -1,14 +1,44 @@
 const botaoAdcionar = document.querySelector('.main-btn')
 const inputTarefa = document.querySelector('.input-bar')
 const listaTarefa = document.querySelector('#todo-list')
+let tarefasSalvas = JSON.parse(localStorage.getItem('itensSalvos')) || []
 
+tarefasSalvas.forEach(tarefa => {
+    adicionarTarefaTela(tarefa)
+});
+
+
+botaoAdcionar.addEventListener('click', () => {
+    adicionarTarefa(inputTarefa.value)
+})
+
+inputTarefa.addEventListener('keydown', (evento) => {
+    if (evento.key === 'Enter') {
+        adicionarTarefa(inputTarefa.value)
+    }
+})
 
 function adicionarTarefa(texto){
-    if (texto !== ''){
+    let novoObjeto = {
+        tarefa: texto,
+        status: false
+    }
+    adicionarTarefaTela(novoObjeto)
+    tarefasSalvas.push(novoObjeto)
+    salvarTarefas()
+}
+
+
+function adicionarTarefaTela(dadoDaTarefa){
+    if (dadoDaTarefa.tarefa !== ''){
         let novaTarefa = document.createElement('li')
             let textoSpan = document.createElement('span')
             textoSpan.classList.add('task-text')
-            textoSpan.innerText = texto
+            textoSpan.innerText = dadoDaTarefa.tarefa
+
+            if (dadoDaTarefa.status == true){
+                novaTarefa.classList.add('task_concluida')
+            }
 
             let botaoExcluir = document.createElement('button')
             botaoExcluir.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
@@ -20,12 +50,16 @@ function adicionarTarefa(texto){
 
             novaTarefa.addEventListener('click', () => {
                 novaTarefa.classList.toggle('task_concluida')
+                dadoDaTarefa.status = !dadoDaTarefa.status
+                salvarTarefas()
             })
 
             botaoExcluir.addEventListener('click', (evento) => {
                 evento.stopPropagation()
                 removerTarefa(novaTarefa)
             })
+
+            
     }
     inputTarefa.value = ''
     
@@ -33,14 +67,13 @@ function adicionarTarefa(texto){
 
 function removerTarefa(tarefa){
     tarefa.remove()
+    let textoDaTarefa = tarefa.querySelector('.task-text').innerText
+    let posicao = tarefasSalvas.indexOf(textoDaTarefa)
+    tarefasSalvas.splice(posicao, 1)
+    salvarTarefas()
 }
 
-botaoAdcionar.addEventListener('click', () => {
-    adicionarTarefa(inputTarefa.value)
-})
+function salvarTarefas(){
+    localStorage.setItem('itensSalvos', JSON.stringify(tarefasSalvas))
+}
 
-inputTarefa.addEventListener('keydown', (evento) => {
-    if (evento.key === 'Enter') {
-        adicionarTarefa(inputTarefa.value)
-    }
-})
